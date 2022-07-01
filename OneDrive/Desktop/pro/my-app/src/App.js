ee70620062f8df4bb9b519953d22fd359be8ef43
UDAYVEERUS <udayveerus348566@gmail.com>
2022-07-01 16:32:35 +0530

    Footer part

---
+++ OneDrive/Desktop/pro/my-app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import React from 'react'
+// import './App.css';
+import Footer from './Footer.js';
+
+
+function App() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ OneDrive/Desktop/pro/my-app/src/Footer.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import "./footer.css"
+
+function Footer(){
+    return(
+        <div>
+            <div>
+  <div className="m_container">
+    <div className="m_middle">
+      <div className="m_middlePart">
+        <div>
+          <h2>Sign Up for Email</h2>
+          <p>
+            Receive early access to new arrivals, sales, exclusive content,
+            events and much more!
+          </p>
+        </div>
+        <div className="m_typePart">
+          <label htmlFor="m_">Email Address</label>
+          <br />
+          <input type="text" />
+          <button>Submit</button>
+        </div>
+      </div>
+      <p>
+        By signing up, you will receive Anthropologie offers, promotions and
+        other commercial messages. You are also agreeing to Anthropologie’s{" "}
+        <a rel="noreferrer">Privacy Policy</a>. You may unsubscribe at any time.
+      </p>
+    </div>
+    <hr />
+    <div className="m_powerPart">
+      <div>
+        <h3>Help</h3>
+        <a rel="noreferrer">Track your order</a>
+        <br />
+        <a rel="noreferrer">Returns &amp; Exchanges</a>
+        <br />
+        <a rel="noreferrer">Shipping</a>
+        <br />
+        <a rel="noreferrer">Customer Service</a>
+        <br />
+        <a rel="noreferrer">Product Recalls</a>
+      </div>
+      <div>
+        <h3>About Us</h3>
+        <a rel="noreferrer">Our Store</a>
+        <br />
+        <a rel="noreferrer">Events</a>
+        <br />
+        <a rel="noreferrer">A Greater Good</a>
+        <br />
+        <a rel="noreferrer">Diversity &amp; Inclusive</a>
+        <br />
+        <a rel="noreferrer">Stories</a>
+      </div>
+      <div>
+        <h3>Services</h3>
+        <a rel="noreferrer">AnthroPerks</a>
+        <br />
+        <a rel="noreferrer">Gift Cards</a>
+        <br />
+        <a rel="noreferrer">Trade Membersship</a>
+        <br />
+        <a rel="noreferrer">Furniture: Guides &amp; Servises</a>
+        <br />
+        <a rel="noreferrer">Klarna</a>
+      </div>
+      <div>
+        <h3>Connect</h3>
+        <a rel="noreferrer">Contact Us</a>
+        <br />
+        <a rel="noreferrer">Stay Connected</a>
+        <br />
+        <a rel="noreferrer">Careers</a>
+        <br />
+        <a rel="noreferrer">Styling Services</a>
+        <br />
+        <a rel="noreferrer">Request A Catalog</a>
+      </div>
+      <div>
+        <a rel="noreferrer">Store Locator</a>
+        <br />
+        <a rel="noreferrer"> Get Email</a>
+      </div>
+    </div>
+    <div className="m_imageLink">
+      <a
+        href="https://www.google.com/url?q=https://play.google.com/&source=gmail-html&ust=1656704151131000&usg=AOvVaw1-ZZSitq1rzlqotej83Be4"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="https://ci6.googleusercontent.com/proxy/yY3lnxYzgy3cUsaNJ5-8iTHerI7DsefIZmyCxvR5HxV-wqL24j2C8Aiqfbn8sRgDMVWJK4wFhm1CptIwIzsP4gg5izGElJ_AbPweFXjdTrskfhJTMs1Cs-v6lkXsM7x1EWiu-n3im7QwD2fq2kkN1oTZfi1B-CYCCQ=s0-d-e1-ft#https://www.freepnglogos.com/uploads/play-store-logo-png/play-store-logo-nisi-filters-australia-11.png"
+          alt=""
+        />
+      </a>
+      <a rel="noreferrer">
+        {" "}
+        <img alt="" />{" "}
+      </a>
+      <a
+        href="https://www.google.com/url?q=https://in.pinterest.com/&source=gmail-html&ust=1656704151131000&usg=AOvVaw0Q_1IhPgZmxtHrJtBrpatZ"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="https://ci6.googleusercontent.com/proxy/omH4cyzpZoY6iaBcI_Q1vlpvFXL9x6nP5WsCc4Ut1y8AV8qJBE7DgKTBpFHmF0gREXQpNFkUGsvtx2fbRLu4-Ho9a-UOf6vj2RQxugC75LzSfLtV8eRCxnEixJoDHYs=s0-d-e1-ft#https://i.pinimg.com/originals/d3/d1/75/d3d175e560ae133f1ed5cd4ec173751a.png"
+          alt=""
+        />
+      </a>
+      <a
+        href="https://www.google.com/url?q=https://twitter.com/home&source=gmail-html&ust=1656704151131000&usg=AOvVaw2ZMYK_89XNOiiiTYmtSyux"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="https://ci5.googleusercontent.com/proxy/Cdup1r_Hk9ibxwP5iTbSF6WDGF4Oeln-eIpyi9_jcrlDBRRPed7LPMQnpntH5aZCPzo-QRvdmSND7TUfvDByejNNLfFd5eElpqopL7pKx8-IcVE3Nq81FwYROzHxmDz8wvxOZb5LBFWQYYi3anY51h5We6WAqHTkAvjdjrVAgr0DR9XRPK-ABw9AFi4BT_4o8V08Dh1J=s0-d-e1-ft#https://static.vecteezy.com/system/resources/previews/002/534/045/original/social-media-twitter-logo-blue-isolated-free-vector.jpg"
+          alt=""
+        />
+      </a>
+    </div>
+    <div className="m_show">
+      <img
+        src="https://ci3.googleusercontent.com/proxy/2W9JuulN5XkC8iWaiCo56J1rST5xK600kD8dh990oEgJde50_ffcxpz9BxKkAcwu-tiB92gbpZbkrQLoPNqw_NRG9AVX1rSe0C_7rhx6Zz1m8YCa_ysUcY1rRPMc-OnMOZ4XsKsk1S2d2kVut10qE34FKE2E1W50m90h-CBqwvCqke8gfcTlWFvY_mof9kUmbNu4G6TT=s0-d-e1-ft#https://images.ctfassets.net/5de70he6op10/2Lh06CXT4AzVnj4LxvGsQD/a424e10a3b8c9e2e270dc22505bfbfa6/022421_SiteFooter_SMS_SIgnUp.jpg"
+        alt=""
+      />
+    </div>
+    <div className="m_lowestPart">
+      <a rel="noreferrer">US</a>
+      <a rel="noreferrer">France</a>
+      <a rel="noreferrer">Germeny</a>
+      <a rel="noreferrer">Italy</a>
+      <a rel="noreferrer">Spain</a>
+      <a rel="noreferrer">UK</a>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+}
+export default Footer;
